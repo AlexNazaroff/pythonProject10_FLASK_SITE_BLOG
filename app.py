e1e10dad0c97fb107919edfa919789ea2a9ab249
AlexNazaroff <ale.krotov@gmail.com>
2021-03-22 12:50:34 +0500

    2

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
+@app.route('/home/new')
 def index():
-    return "Hello World"
+    return "E-mail"
 
+@app.route('/user/<string:name>/<int:id>')
+def index(name,id):
+    return "User Page" + name +'' + id
 
 @app.route('/about')
 def about():
