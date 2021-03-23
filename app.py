09544efda77a80f3101af3a2b7b3f3298f1eb5a2
AlexNazaroff <ale.krotov@gmail.com>
2021-03-23 15:24:08 +0500

    11 SQL

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -26,20 +26,35 @@ def index():
     return render_template('index.html')
 
 
-
-
 @app.route('/user/<string:name>/<int:id>')
-def user(name,id):
+def user(name, id):
     return "User Page: " + name + ' ' + str(id)
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/create-article')
-def create_article():
-    return render_template('create-article.html')
 
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+
+
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'При добавлении статьи произошла ошибка '
+    else:
+
+        return render_template('create-article.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
