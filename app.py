b34e009b7b346828315db186a638ac35f99b8e5c
AlexNazaroff <ale.krotov@gmail.com>
2021-03-24 12:48:07 +0500

    16 Base delete element SQL

--- app.py
+++ app.py
@@ -50,6 +50,17 @@ def post_detail(id):
     return render_template('post_detail.html', article=article)
 
 
+@app.route('/posts/<int:id>/del')
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return 'При удалении статьи произошла ошибка'
+
 
 @app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
